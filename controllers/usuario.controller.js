fb29ea1a41df9b16f8fcbabac38135bd0ffe7940
CarlosBGG <carlos.benavides@epn.edu.ec>
2024-12-18 21:23:37 -0500

    Usuario Backend

--- controllers/usuario.controller.js
+++ controllers/usuario.controller.js
@@ -6,13 +6,29 @@ module.exports.createUsuario = (request, response) => {
     Usuario.create({
         email, password
     })
-        .then(usuario => response.status(200).json(usuario.email))
+        .then(usuario => response.status(200).json({email:usuario.email}))
         .catch(err => response.status(400).json(err));
 }
 
+
 module.exports.getUsuario = (request, response) => {
     const { email, password } = request.body;
-    Restaurante.findOne({email: email, })
-        .then(restaurante => response.status(200).json(restaurante))
-        .catch(err => response.status(500).json(err))
-}
+
+    Usuario.findOne({ email: email, password: password })
+        .then(usuario => {
+            if (usuario) {
+                // Retorna únicamente el email si las credenciales son correctas
+                response.status(200).json({
+                    email: usuario.email
+                });
+            } else {
+                response.status(401).json({ // Código 401 para credenciales inválidas
+                    message: 'Invalid email or password'
+                });
+            }
+        })
+        .catch(err => response.status(500).json({
+            message: 'Internal server error',
+            error: err
+        }));
+};
